6e4dd7b5cd408d1976f02bdb2232fe8ff540852f
yuchan choe <youcank20@gmail.com>
2022-08-05 23:35:28 +0900

    enemy targeting improvement

--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -11,6 +11,8 @@ public class PlayerAttack : MonoBehaviour
 
     private int _targetIndex;
     private float _targetDistance;
+    private int _closestIndex;
+    private float _closestDistance;
     private Rigidbody _rigidbody;
     private PlayerMovement _playerMovement;
 
@@ -28,16 +30,43 @@ public class PlayerAttack : MonoBehaviour
         {
             _targetIndex = -1;
             _targetDistance = MAX_DISTANCE;
+            _closestIndex = -1;
+            _closestDistance = MAX_DISTANCE;
 
             for (int i = 0; i < EnemyList.Count; ++i)
             {
                 float enemyDistance = Vector3.Distance(EnemyList[i].transform.position, transform.position);
 
-                if (enemyDistance <= _targetDistance)
+                if (enemyDistance <= MAX_DISTANCE)
                 {
-                    _targetDistance = enemyDistance;
-                    _targetIndex = i;
+                    RaycastHit hit;
+                    bool isHit = Physics.Raycast(transform.position + new Vector3(0f, 0.5f, 0f), EnemyList[i].transform.position - transform.position, out hit, MAX_DISTANCE);
+
+                    if (isHit && hit.collider.CompareTag("Enemy"))
+                    {
+                        if (enemyDistance <= _targetDistance)
+                        {
+                            _targetDistance = enemyDistance;
+                            _targetIndex = i;
+                        }
+                    }
+                    else
+                    {
+                        if (enemyDistance <= _closestDistance)
+                        {
+                            _closestDistance = enemyDistance;
+                            _closestIndex = i;
+                        }
+                    }
                 }
+            }
+
+            if (_targetIndex == -1)
+            {
+                if (_closestIndex == -1)
+                    HasTarget = false;
+                else
+                    _targetIndex = _closestIndex;
             }
 
             if (_targetIndex != -1)
@@ -46,8 +75,6 @@ public class PlayerAttack : MonoBehaviour
 
                 _rigidbody.rotation = Quaternion.LookRotation(EnemyList[_targetIndex].transform.position - transform.position);
             }
-            else
-                HasTarget = false;
         }
         else
             HasTarget = false;
