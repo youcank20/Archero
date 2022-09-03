9693705c173d14c8c5beddf2938ce0cb5df69892
yuchan choe <youcank20@gmail.com>
2022-09-03 23:55:40 +0900

    save load

---
+++ Assets/Scripts/Manager/DataManager.cs
@@ -0,0 +1,42 @@
+using System.IO;
+using UnityEngine;
+
+public class DataManager : MonoBehaviour
+{
+    private static DataManager _instance;
+
+    public static DataManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+                _instance = FindObjectOfType<DataManager>();
+
+            return _instance;
+        }
+    }
+
+    private PlayerData _playerData;
+    private string _path;
+    private string _fileName;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+
+        _path = Application.persistentDataPath + "/";
+        _fileName = "playerdata";
+    }
+
+    public void SaveData()
+    {
+        string contents = JsonUtility.ToJson(_playerData);
+        File.WriteAllText(_path + _fileName, contents);
+    }
+
+    public void LoadData()
+    {
+        string contents = File.ReadAllText(_path + _fileName);
+        _playerData = JsonUtility.FromJson<PlayerData>(contents);
+    }
+}
---
+++ Assets/Scripts/Player/PlayerData.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class PlayerData
+{
+    public int Level;
+    public int Experience;
+    public int Energy;
+    public int Coin;
+    public int Diamond;
+}
