bf4cfc79af978e841ca44cb7fe88efd0812f9671
yuchan choe <youcank20@gmail.com>
2022-09-12 23:29:55 +0900

    enemy_06

---
+++ Assets/Scripts/Creature/Enemy/Dragon.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class Dragon : Enemy
+{
+    private void Start()
+    {
+        maxHp = 10;
+        currentHp = 10;
+    }
+}
---
+++ Assets/Scripts/Creature/Enemy/SkeletonArcher.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class SkeletonArcher : Enemy
+{
+    private void Start()
+    {
+        maxHp = 10;
+        currentHp = 10;
+    }
+}
